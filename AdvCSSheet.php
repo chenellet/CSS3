eec0dce806b1686010ef29df879e578f5da50dd6
Chenelle Travis <chenelle.l.t@gmail.com>
2020-02-02 17:34:25 -0500

    AdvCSSheet for PHP
    Withdraw all learned elements from text file for reduced sorting time.

---
+++ AdvCSSheet.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="EN">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8">
+    <link rel="stylesheet" media="all" href="./AdvSelectorLesson.css">
+</head>
+    <body>
+        <table class="tabular-data">
+            <thead>
+            <tr><td colspan="3">Pseudo Selectors</td colspan="3"></tr>
+            <tr>
+            <th>Example</th>
+            <th>Classification</th>
+            <th>Explanation</th>
+            </tr>
+            </thead>
+            <tbody>
+                <?php 
+                    error_reporting(0);
+                    $array = $fields = array(); $i=0;
+                    $handle = @fopen('./CSS3dataList.txt','r');
+
+                    if ($handle) {
+                        while (($row = fgetcsv($handle, 4096, ',')) !== false) {
+                            if (empty($fields)) {
+                                $fields = $row;
+                                continue;
+                            }
+                            foreach ($row as $k=>$value) {
+                                $array[$i][$fields[$k]] = $value;
+                            }
+                            $i++;
+                        }
+
+                        if (!feof($handle)) {
+                            echo "Error: unexpected fgets() fail\n";
+                        }
+                        fclose($handle);
+                    }
+                    for($x=0;$x < count($array); $x++){
+                        echo '<tr><td data-title="Example"><code>'.$array[$x]['Example'].'</code></td><td data-title="Description">'.$array[$x]['Description'].'</td><td data-title="Explanation">'.$array[$x]['Explanation'].'</td></tr>';                 
+                    }
+
+                
+                ?>
+            </tbody>
+            </table>
+        </body>
+</html>
